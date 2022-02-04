75c11580226cf0ddfd2d3d55e6e62cf4e59d1d3a
고영빈 <skekq123@naver.com>
2022-02-04 13:55:22 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hanghae_spring3
+
 스프링 주특기공부3
---
+++ src/main/java/com/sparta/hanghae_spring3/HanghaeSpring3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghae_spring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeSpring3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeSpring3Application.class, args);
+    }
+
+}
